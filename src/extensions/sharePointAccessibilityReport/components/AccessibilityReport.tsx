43f3ed54b15bd3ce441d2050524705879218c516
Vadim Stakhnyuk (CBRE INC) <v-vstakhnyuk@microsoft.com>
2024-01-08 09:35:04 -0800

    Commiting Local Changes to push updates

--- src/extensions/sharePointAccessibilityReport/components/AccessibilityReport.tsx
+++ src/extensions/sharePointAccessibilityReport/components/AccessibilityReport.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import * as axe from 'axe-core';
-import "./AccessibilityReport.module.scss";
+import styles from "./AccessibilityReport.module.scss";
+
 
 export interface IAccessibilityReportProps {
 }
 
 export interface IAccessibilityReportState {
     data: Array<any>;
-    showChatBot: boolean;
+    currentPage:number;
+    issuePerPage:number;
+    isPrevBtnActive:string;
+    isNextBtnActive:string;
 }
 
 export class AccessibilityReport extends React.Component<IAccessibilityReportProps, IAccessibilityReportState> {
@@ -15,8 +19,15 @@ export class AccessibilityReport extends React.Component<IAccessibilityReportPro
         super(props);
         this.state = {
             data: [],
-            showChatBot: false,
+            currentPage: 1,
+            issuePerPage: 1,
+            isPrevBtnActive: 'disabled',
+            isNextBtnActive: ''
         };
+        this.handleClick = this.handleClick.bind(this);
+        this.btnNextClick = this.btnNextClick.bind(this);
+        this.btnPrevClick = this.btnPrevClick.bind(this);
+        this.setPrevAndNextBtnClass = this.setPrevAndNextBtnClass.bind(this);
     }
 
     componentDidMount(): void {
@@ -26,11 +37,165 @@ export class AccessibilityReport extends React.Component<IAccessibilityReportPro
         }, 100)
     }
 
+    getAccessbility() {
+        // ToDo: add error message in panel
+        axe
+            .run(".CanvasComponent")
+            .then(async results => {
+                if (results.violations.length) {
+                    this.setState({ data: results.violations });
+                    console.log("Accessibility Violations", this.state.data);
+                }
+                else {
+                    console.log("No violations")
+                }
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    }
+
+    handleClick(event: any) {
+        let listid = Number(event.target.id)
+        this.setState({
+            currentPage: Number(event.target.id)
+        })
+        this.setPrevAndNextBtnClass(listid);
+    }
+
+    setPrevAndNextBtnClass(listid: any) {
+        let totalPage = Math.ceil(this.state.data.length / this.state.issuePerPage);
+        this.setState({isNextBtnActive: 'disabled'});
+        this.setState({isPrevBtnActive: 'disabled'});
+        if(totalPage === listid && totalPage > 1){
+            this.setState({isPrevBtnActive: ''});
+        }
+        else if(listid === 1 && totalPage > 1){
+            this.setState({isNextBtnActive: ''});
+        }
+        else if(totalPage > 1){
+            this.setState({isNextBtnActive: ''});
+            this.setState({isPrevBtnActive: ''});
+        }
+    }
+
+    btnPrevClick() {
+        let listid = this.state.currentPage - 1;
+        this.setState({currentPage: listid});
+        this.setPrevAndNextBtnClass(listid);
+    }
+
+    btnNextClick() {
+        let listid = this.state.currentPage + 1;
+        this.setState({currentPage: listid});
+        this.setPrevAndNextBtnClass(listid);
+    }
+
     public render(): React.ReactElement<IAccessibilityReportProps> {
+
+        const {data, currentPage, issuePerPage, isNextBtnActive, isPrevBtnActive} = this.state;
+        
+        const indexOfLastIssue = currentPage * issuePerPage;
+        const indexOfFirstIssue = indexOfLastIssue - issuePerPage;
+        const currentIssues = data.slice(indexOfFirstIssue, indexOfLastIssue);
+
+        const renderIssues = currentIssues.map((issue, index) => {
+            return (
+                <div key={index}>
+                    <div className="AccessibilityReportHeader">
+                        <h3>Element with Accessibility Issue</h3>
+                        <p>{issue.id}</p>
+                    </div>
+                    <div className="AccessibilityReportBody">
+                        <div className="simpleDescription">
+                            <h4>Simple Description of Issue</h4>
+                            <p>{issue.help}</p>
+                        </div>
+                        <div className="description">
+                            <h4>Description</h4>
+                            <p>{issue.description}</p>
+                        </div>
+                        <div className="impact">
+                            <h4>Impact</h4>
+                            <p>{issue.impact}</p>
+                        </div>
+                        <div className="helpUrl">
+                            <h4>Helpful Link</h4>
+                            <a href={issue.helpUrl} target="_blank" rel="noopener noreferrer">{issue.helpUrl}</a>
+                        </div>
+                        <div>
+                            <h3>Instances of Accessibility Issues</h3>
+                            {issue.nodes.map(function (node: any, index: any) {
+                                return (
+                                    <ul>
+                                        <li key={index}>
+                                            <span><h3>Failure Summary : </h3><p>{node.failureSummary}</p></span>
+                                            <span><h3>HTML : </h3><p>{node.html}</p></span>
+                                            <span><h3>Impact : </h3><p>{node.impact}</p></span>
+                                        </li>
+                                    </ul>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+
+        let renderPrevButton = null;
+        if(isPrevBtnActive === 'disabled') {
+            renderPrevButton = (
+                <div className="prevButton">
+                    <span id="btnPrev"> Prev </span>
+                </div>
+            )
+        }
+        else {
+            renderPrevButton = (
+                <div className="prevButton">
+                    <a href="#" id="btnPrev" onClick={this.btnPrevClick}> Prev </a>
+                </div>
+            )
+        }
+        let renderNextBtn = null;
+        if(isNextBtnActive === 'disabled') {
+            renderNextBtn = (
+                <div className="nextButton">
+                    <span id="btnNext"> Next </span>
+                </div>
+            )
+        }
+        else {
+            renderNextBtn = (
+                <div className="nextButton">
+                    <a href="#" id="btnNext" onClick={this.btnNextClick}> Next </a>
+                </div>
+            )
+        }
+
         return (
+            <div data-id="menuPanel" className="AccessibilityReport">
+                <div>
+                    {renderIssues}
+                </div>
+                <div className={styles.pagination}>
+                    <div>
+                        {renderPrevButton}
+                    </div>
+                    <div>
+                        {renderNextBtn}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+        /*return (
             <div data-id="menuPanel" id="AccessibilityReport">
                 {this.state.data.map(issue => {
-                    return (
                         <div>
                             <div className="AccessibilityReportHeader">
                                 <h3>Element with Accessibility Issue</h3>
@@ -61,32 +226,15 @@ export class AccessibilityReport extends React.Component<IAccessibilityReportPro
                             </div>
                             <p>----------------------------------------------------------------------------------</p>
                         </div>
-                    )
-                })}
-
+                    })
+                })
             </div>
-        );
-    }
+        );*/
 
-    private getAccessbility() {
-        // ToDo: add error message in panel
-        axe
-            .run(".CanvasComponent")
-            .then(async results => {
-                if (results.violations.length) {
-                    this.setState({ data: results.violations });
-                    console.log("Accessibility Violations", this.state.data);
-                }
-                else {
-                    console.log("No violations")
-                }
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
-    }
 
-}
+
+
+
 
 /*                                {issue.nodes.map((issueNode: { html: any; }) => 
                                     <p>{issueNode.html}</p>
